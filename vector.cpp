1858506471f372b7c0c0b18b4dbc8976ce1aebf3
System Administrator <root@vic-mcgowens-macbook.local>
2014-02-17 17:52:37 -0500

    This is the empty vector cpp file

--- vector.cpp
+++ vector.cpp
@@ -3,8 +3,24 @@
 
 #include "vector.h"
 
-Vector::Vector() :  
+Vector::Vector() :  {}
 
+Vector::Vector(double x, double y, double z{}
 
+Vector::Vector(const Vector& other){}
+
+Vector::Vector(Eigen::Vector3d vec){}
+
+Vector::transform(Eigen::Matrix4d){}
+
+Vector Vector::operator+(const Vector& v1, const Vector& v2) const{}
+
+Vector Vector::operator-(const Vector& v1, const Vector& v2) const{}
+
+Vector Vector::operator*(const Vector& v1, const Vector& v2) const{}
+
+Vector Vector::operator**(const Vector& v1, const Vector& v2) const{}
+
+double& operator[](const int index) const{}
 
 
