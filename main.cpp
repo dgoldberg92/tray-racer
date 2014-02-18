147a26ceb97e751a8d8bd30691a74bb696e23ac6
Daniel S. Goldberg <DGoldberg92@gmail.com>
2014-02-17 19:40:28 -0500

    Tried to make CMakeLists.txt and main tester

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "colour.h"
+
+int main() {
+  
+  Colour c0(.8,.8,.8);
+  Colour c1(.5,.5,.5);
+  
+  Colour c2 = c0*c1;
+  
+  std::cout << c2.getR() << ", " << c2.getG() << ", " << c2.getB() << std::endl;
+  return 0;
+}
+
