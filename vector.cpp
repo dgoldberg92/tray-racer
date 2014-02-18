7e54959f57645562d545848847739a577b07477c
Daniel S. Goldberg <DGoldberg92@gmail.com>
2014-02-17 20:32:01 -0500

    added implementation to vector and fixed some stuff in .h

--- vector.cpp
+++ vector.cpp
@@ -3,24 +3,56 @@
 
 #include "vector.h"
 
-Vector::Vector() :  {}
+Vector::Vector()
+  : vector_(Eigen::Vector3d())  {}
 
-Vector::Vector(double x, double y, double z{}
+Vector::Vector(double x, double y, double z)
+  : vector_(Eigen::Vector3d(x,y,z)) {}
 
-Vector::Vector(const Vector& other){}
+Vector::Vector(const Vector& other)
+  : vector_(other.getVector()){}
 
-Vector::Vector(Eigen::Vector3d vec){}
+Vector::Vector(const Eigen::Vector3d vec)
+  : vector_(vec){}
 
-Vector::transform(Eigen::Matrix4d){}
+void Vector::transform(const Eigen::Matrix4d mat){
+  Eigen::Vector4d v(vector_(0),vector_(1),vector_(2),1.0);
+  Eigen::Vector4d newV = mat*v;
+  vector_(0) = newV(0);
+  vector_(1) = newV(1);
+  vector_(2) = newV(2);
+}
 
-Vector Vector::operator+(const Vector& v1, const Vector& v2) const{}
+Vector Vector::operator+(const Vector& v1, const Vector& v2) const{
+  
+  return Vector(v1(0)+v2(0),v1(1)+v2(1),v1(2)+v2(2));
 
-Vector Vector::operator-(const Vector& v1, const Vector& v2) const{}
+}
 
-Vector Vector::operator*(const Vector& v1, const Vector& v2) const{}
+Vector Vector::operator-(const Vector& v1, const Vector& v2) const{
+  return v1+(-1)*v2;
+}
 
-Vector Vector::operator**(const Vector& v1, const Vector& v2) const{}
+Vector Vector::operator*(const Vector& v1, const Vector& v2) const{
+  return Vector(v1.getVector()*v2.getVector());
+}
 
-double& operator[](const int index) const{}
+Vector Vector::operator*(const double& a, const Vector& v1) const{
+  return Vector(v1(0)*a,v1(1)*a,v1(2)*a);
+}
+
+Vector Vector::operator*(const Vector& v1, const double& a) const{
+  return Vector(v1(0)*a,v1(1)*a,v1(2)*a);
+}
+
+Vector Vector::operator**(const Vector& v1, const Vector& v2) const{
+  return (v1.getVector()).cross(v2.getVector());
+}
+
+double& operator()(const int index) const{
+  assert(index >= 0 && index<3);
+
+  return vector_(index);
+}
 
 
--- vector.h
+++ vector.h
@@ -11,21 +11,24 @@ class Vector {
     Vector();
     Vector(double x, double y, double z);
     Vector(const Vector& other);
-    Vector(Eigen::Vector3d vec);
+    Vector(const Eigen::Vector3d vec);
     virtual ~Vector();
 
-    double getX() const {return vector_[0]};
-    double getY() const {return vector_[1]};
-    double getZ() const {return vector_[2]};
+    double getX() const {return vector_(0)};
+    double getY() const {return vector_(1)};
+    double getZ() const {return vector_(2)};
+    Eigen::Vector3d getVector() const {return vector_};
     double getLength() const {return vector_.norm()};
     void normalize() {return vector_.normalize()};
-    void transform(Eigen::Matrix4d);
+    void transform(const Eigen::Matrix4d);
 
     Vector operator+(const Vector& v1, const Vector& v2) const;
     Vector operator-(const Vector& v1, const Vector& v2) const;
     Vector operator*(const Vector& v1, const Vector& v2) const;
+    Vector operator*(const Vector& v1, const double a) const;
+    Vector operator*(const double a, const Vector& v1) const;
     Vector operator**(const Vector& v1, const Vector& v2) const;
-    double& operator[](const int index) const;
+    double& operator()(const int index) const;
 
   private:
     Eigen::Vector3d vector_;
