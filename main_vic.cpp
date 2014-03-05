60d04246ea15739b8428ca8428e6c0d79b2f6015
Vic <vkm3473@rit.edu>
2014-03-04 22:44:45 -0500

    Changed main_vic to get correct image output

--- main_vic.cpp
+++ main_vic.cpp
@@ -14,15 +14,11 @@ int main() {
   
  // Object* o1 = new Sphere(1., Point(-4.5, 4.45, -3.05));
 //Object* o1 = new Sphere(0.81, Point(17.89, 10.51, -5.65));
-  Object* o1 = new Sphere(10, Point(0, 0, 20));
+  Object* o1 = new Sphere(5, Point(-2, 2, 6));
   o1->setColour(Colour(1.,0.,0.));
 
-  Object* o2 = new Sphere(10, Point(25, 0, 25));
+  Object* o2 = new Sphere(5, Point(5, 0, 8));
   o2->setColour(Colour(0.,1.,0.));
-
-  Object* o3 = new Sphere(2, Point(0, 10, 0));
-  o3->setColour(Colour(1.,0.,0.));
-
 
   //Point topLeft(-2, 2, -2000);//6., -2., 0.);
   //Point topRight(2,2,-2000);//6., 2., 0.);
@@ -30,24 +26,22 @@ int main() {
   //Point botRight(2,-2,-2000);//4., -2., 0.);
   
 // TESTING
-  Point topLeft(-16, 5, 20);//6., -2., 0.);
-  Point topRight(16,5, 20);//6., 2., 0.);
-  Point botLeft(-16,0, 20);//,4., 2., 0.);
-  Point botRight(-16,0, 20);//,4., 2., 0.);
-  Object* t1 = new Triangle(topLeft, topRight, botLeft);
+ Point topLeft(-32, 2, 25);//6., -2., 0.);
+ Point topRight(45,2, 25);//6., 2., 0.);
+ Point botLeft(-32,-20, 5);//,4., 2., 0.);
+ Point botRight(40,-20, 5);//,4., 2., 0.);
+ 
 
-  //Object* t1 = new Triangle(topLeft, topRight, botLeft);
+  Object* t1 = new Triangle(topLeft, topRight, botLeft);
   Object* t2 = new Triangle(topRight, botRight, botLeft);
   
   t1->setColour(Colour(0.,0.,1.));
   t2->setColour(Colour(0.,0.,1.));
 
   w.add(t1);
-  //w.add(t2);
-  //w.add(o1);
-  //w.add(o2);
-  //w.add(o3);
-  //w.add(o2);
+  w.add(t2);
+  w.add(o1);
+  w.add(o2);
 
   //Point camPos(-6.01, 13.44, -2.59);
   //Point camPos(-1.8, 2.33, -10.32);
@@ -59,9 +53,9 @@ int main() {
   //Camera cam(camPos,look,up, 3.5);
   Camera cam(camPos,look,up, .5);
   cam.setPixDim(720,540);
-  //cam.setPixDim(540,720);
   cam.setDim(4,3);
-  
+
+
   Image im = cam.render(w);
   
   im.toPPM("test.ppm");
