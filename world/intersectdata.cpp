a9770f74c7729f208cf92973f2d6ab322d12492e
Vic <vkm3473@rit.edu>
2014-03-06 21:37:42 -0500

    Beginnings of IlluminationModel abstract class

--- world/intersectdata.cpp
+++ world/intersectdata.cpp
@@ -3,9 +3,9 @@
 #include "intersectdata.h"
 
 IntersectData::IntersectData()
-	: point_(Point()), normal_(Vector()), incoming_(Point()), reflective_(Point()){}
+	: point_(Point()), normal_(Vector()), incoming_(Ray()), reflective_(Ray()){}
 
-IntersectData::IntersectData(Point point, Vector normal, Point incoming, Point reflective)
+IntersectData::IntersectData(Point point, Vector normal, Ray incoming, Ray reflective)
 	: point_(point), normal_(normal), incoming_(incoming), reflective_(reflective){}
 
 IntersectData::~IntersectData(){} // deconstructor
@@ -16,10 +16,10 @@ void IntersectData::setPoint(Point point){
 void IntersectData::setNormal(Vector normal){
 	normal_ = normal;
 }
-void IntersectData::setIncoming(Point incoming){
+void IntersectData::setIncoming(Ray incoming){
 	incoming_ = incoming;
 }
-void IntersectData::setReflective(Point reflective){
+void IntersectData::setReflective(Ray reflective){
 	reflective_ = reflective;
 }
 void IntersectData::setLightList(std::list<Light*> lights){
--- world/intersectdata.h
+++ world/intersectdata.h
@@ -16,28 +16,28 @@ class IntersectData{
 public:
 
 	IntersectData(); // empty constructor
-	IntersectData(Point point, Vector normal, Point incoming, Point reflective);
+	IntersectData(Point point, Vector normal, Ray incoming, Ray reflective);
 	virtual ~IntersectData(); // deconstructor
 
 	// getters
 	Point getPoint()const {return point_;};
 	Vector getNormal()const {return normal_;};
-	Point getIncoming()const {return incoming_;};
-	Point getReflective()const {return reflective_;};
+	Ray getIncoming()const {return incoming_;};
+	Ray getReflective()const {return reflective_;};
 	std::list<Light*> getLightList() const {return lights_;};
 
 	// setters
 	void setPoint(Point point);
 	void setNormal(Vector normal);
-	void setIncoming(Point incoming);
-	void setReflective(Point reflective);
+	void setIncoming(Ray incoming);
+	void setReflective(Ray reflective);
 	void setLightList(std::list<Light*> lights);
 
 private:
 	Point point_;
 	Vector normal_;
-	Point incoming_;
-	Point reflective_;
+	Ray incoming_;
+	Ray reflective_;
 	std::list<Light*> lights_;
 };
 
