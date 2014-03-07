8b93224e5187b7905acc8036bd05e3504cd6ef45
Daniel S. Goldberg <DGoldberg92@gmail.com>
2014-03-06 21:02:57 -0500

    fixed addModel()

--- world/object.h
+++ world/object.h
@@ -21,7 +21,7 @@ public:
 	virtual double intersect(const Ray& b) const = 0; // initial intersect method
 	virtual void transform(const Eigen::Matrix4d& mat) = 0; // transform method
   virtual std::string toString() const = 0;
-  void setModel(const IlluminationModel) = 0;
+  void setModel(const IlluminationModel& m) = 0;
 	void setColour(const Colour& c){colour_=c;};
   Colour getColour(const Ray& r, double w) const = 0;
 
